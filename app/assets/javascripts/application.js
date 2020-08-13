9e1c09cb1314bcc3480ca25f68873ed0a2c47a4d
Wei Kang <wei.kang8775@gmail.com>
2020-08-12 20:24:32 -0600

    Add favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require turbolinks
 //= require_tree .
+//= require semantic-ui
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+})
