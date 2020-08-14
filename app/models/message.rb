de257f4a4b6c61e55ac3402b364bde35dc47adfc
Wei Kang <wei.kang8775@gmail.com>
2020-08-13 20:25:13 -0600

    Add login logic

---
+++ app/models/message.rb
@@ -0,0 +1,4 @@
+class Message < ApplicationRecord
+  validates :body, presence: true, length: { maximum: 200 }
+  belongs_to :user
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Manaka", password: "manaka")
+User.create(username: "Rinne", password: "rinne")
+User.create(username: "Byakuya", password: "byakuya")
